dbc478de61dc17c4d46c3effba1f46852958a8ce
Srikanth Anusuri <sanusuri-cw@starbucks.com>
2018-01-11 18:24:44 -0800

    Added logic to write the order data to a writer object.

--- src/main/java/com/example/pizza_shop/service/OrderService.java
+++ src/main/java/com/example/pizza_shop/service/OrderService.java
@@ -4,11 +4,14 @@ import com.example.pizza_shop.exception.InvalidFileException;
 import com.example.pizza_shop.model.Order;
 import com.example.pizza_shop.model.Topping;
 import com.example.pizza_shop.model.ToppingBuilder;
+import com.example.pizza_shop.service.comparator.ToppingComparator;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
+import java.text.MessageFormat;
+import java.util.Collections;
 import java.util.List;
 
 public class OrderService {
@@ -30,6 +33,21 @@ public class OrderService {
             throw new InvalidFileException("No toppings present");
         }
         return order;
+    }
+
+    public void writeOrder(Order order, Writer writer) throws IOException {
+        if(order == null || writer == null) {
+            throw new IllegalArgumentException(MessageFormat.format("Arguments cannot be null; Order: [{0}], Writer: [{1}]", order, writer));
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(writer); PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+            printWriter.println(order.getHeader());
+            List<Topping> toppings = order.getToppings();
+            Collections.sort(toppings, new ToppingComparator());
+            for (Topping topping : toppings) {
+                printWriter.println(topping.toHumanReadableFormat());
+            }
+        }
     }
 
     private void addToppings(List<String> items, Order order) {
--- src/test/java/com/example/pizza_shop/service/OrderServiceTest.java
+++ src/test/java/com/example/pizza_shop/service/OrderServiceTest.java
@@ -2,12 +2,15 @@ package com.example.pizza_shop.service;
 
 import com.example.pizza_shop.exception.InvalidFileException;
 import com.example.pizza_shop.model.Order;
+import com.example.pizza_shop.model.Topping;
+import com.example.pizza_shop.model.ToppingBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -59,5 +62,37 @@ public class OrderServiceTest {
 
         //act
         orderService.readOrder(new StringReader(content));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void writeOrderWhenOrderIsNull() throws Exception {
+        orderService.writeOrder(null, new StringWriter());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void writeOrderWhenWriterIsNull() throws Exception {
+        orderService.writeOrder(new Order("header"), null);
+    }
+
+    @Test
+    public void writeOrder() throws Exception {
+        Order order = new Order("header");
+        StringWriter writer = new StringWriter();
+
+        Topping veggie = new ToppingBuilder("Veggie\t\t1474295087").createTopping();
+        order.getToppings().add(veggie);
+
+        Topping meat = new ToppingBuilder("Meat\t\t1506176687").createTopping();
+        order.getToppings().add(meat);
+
+        //act
+        orderService.writeOrder(order, writer);
+
+        String[] lines = writer.toString().split(System.lineSeparator());
+
+        assertThat(lines).hasSize(3);
+        assertThat(lines[0]).isEqualTo("header");
+        assertThat(lines[1]).containsPattern("Meat");
+        assertThat(lines[2]).containsPattern("Veggie");
     }
 }
